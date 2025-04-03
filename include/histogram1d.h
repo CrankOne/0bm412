525fe45f0ef88177385b9b1c1e311b52f8641faa
Renat Dusaev <rdusaev@cern.ch>
2025-04-03 13:03:21 +0200

    Drafted accept/reject implementation

--- include/histogram1d.h
+++ include/histogram1d.h
@@ -6,7 +6,12 @@ struct Histogram1D {
     float rangeLength;
 };
 
+// intializes histogram instance
 void histogram_init(struct Histogram1D * h, float rangeMin, float rangeMax);
+
+// adds value to histogram
 void histogram_fill(struct Histogram1D * h, float v );
+
+// prints histogram instance
 void histogram_print(struct Histogram1D * h);
 
--- main.c
+++ main.c
@@ -1,18 +1,43 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "include/histogram1d.h"
-//#include <math.h>
+#include <math.h>
 //#include <time.h>
+
+float func(float x) {
+    return exp( - x*x/2.5);
+}
+
+float accept_reject( float xMin, float xMax, float yMin, float yMax
+             , float (*pdf)(float x)
+             ) {
+    do {
+        int u1 = rand()
+          , u2 = rand()
+          ;
+        float x = (u1/((float) RAND_MAX))*(xMax - xMin) + xMin;
+        float y = (u2/((float) RAND_MAX))*(yMax - yMin) + yMin;
+
+        if(y <= pdf(x)) return x;
+    } while(1);
+}
 
 int main() {
     struct Histogram1D h;
-    histogram_init(&h, -0.23, 1.45);
+
+    float rangeX[2], rangeY[2];
+    rangeX[0] = - 5;
+    rangeX[1] =   5;
+    rangeY[0] = 0;
+    rangeY[1] = 1;
+
+    histogram_init(&h, rangeX[0], rangeX[1]);
 
     for(int i = 0; i < 1e6; ++i ) {
-        int u = rand();
-        float a = 1.23*((float) u)/RAND_MAX - 0.15;
+        float x = accept_reject(rangeX[0], rangeX[1]
+                , rangeY[0], rangeY[1], func);
 
-        histogram_fill(&h, a);
+        histogram_fill(&h, x);
     }
 
     histogram_print(&h);
